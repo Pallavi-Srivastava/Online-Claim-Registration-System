001f0b025e509cdb8959d1a865738554c5d2ca53
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-12-29 17:58:23 +0530

    Added:Initial Setup for Project

---
+++ src/main/java/com/blz/onlineclaimregistartion/OnlineClaimRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.blz.onlineclaimregistartion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineClaimRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineClaimRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blz/onlineclaimregistartion/OnlineClaimRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blz.onlineclaimregistartion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineClaimRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

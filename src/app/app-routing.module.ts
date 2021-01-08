7ee88a7d8b3f6aebaf0dad1be26e42b955e5869c
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2021-01-08 16:26:14 +0530

    Added:Code to redirect each component.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { LoginComponent } from './components/login/login.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { ClaimSideNavbarComponent } from './components/claim-side-navbar/claim-side-navbar.component';
+import { PolicySideNavbarComponent } from './components/policy-side-navbar/policy-side-navbar.component';
 
 const routes: Routes = [
-  // {path:"home-page",component:HomePageComponent},
   {path:"",component:LoginComponent},
-  {path:"header",component:HeaderComponent}
+  {path:"header",component:HeaderComponent},
+  {path:"claim-page",component:ClaimSideNavbarComponent},
+  {path:"policy-page",component:PolicySideNavbarComponent}
+  
   // {path: "landing-page/:id", component: EmployeePayrollFormComponent}
 ];
 
